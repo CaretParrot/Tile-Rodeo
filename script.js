8826a0518eafec4d5810106656b28dbc6f1563e8
CaretParrot <wopow29@gmail.com>
2024-05-29 20:47:57 -0500

    Added layout for timeSelector and setup basic layout index.html

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function switchPage(pageId) {
+    let allPages = document.getElementsByClassName("page");
 
+    for (let i = 0; i < allPages.length; i++) {
+        allPages[i].style.display = "none";
+    }
+
+    id(pageId).style.display = "flex";
+}
