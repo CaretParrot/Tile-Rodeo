109dbda10df5f0193116452eda45c47882814c3b
CaretParrot <wopow29@gmail.com>
2024-05-31 18:39:24 -0500

    Finished main functionality of application index.html

--- script.js
+++ script.js
@@ -6,11 +6,24 @@ let timerRunning = false;
 let whitesTurn = null;
 let timer;
 
-setClocks();
+setClocks(id("daysTime").value, id("hoursTime").value, id("minutesTime").value, id("secondsTime").value, id("daysTime").value, id("hoursTime").value, id("minutesTime").value, id("secondsTime").value);
 
 let timerData = {
-    whiteTotalTime: null,
-    blackTotalTime: null
+    increment: null,
+    white: {
+        totalTime: null,
+        days: null,
+        hours: null,
+        minutes: null,
+        seconds: null
+    },
+    black: {
+        totalTime: null,
+        days: null,
+        hours: null,
+        minutes: null,
+        seconds: null
+    }
 }
 
 function switchPage(pageId) {
@@ -27,70 +40,133 @@ function toggleClock() {
     if (timerRunning === false) {
         timerRunning = true;
         whitesTurn = true;
-        timerData.whiteTotalTime = +id("daysTime").value + +id("hoursTime").value + +id("minutesTime").value + +id("secondsTime").value;
-        timerData.blackTotalTime = +id("daysTime").value + +id("hoursTime").value + +id("minutesTime").value + +id("secondsTime").value;
-        id("middleButton").innerHTML = "Reset";
+        timerData.white.totalTime = (+id("daysTime").value * 86400) + (+id("hoursTime").value * 3600) + (+id("minutesTime").value * 60) + +id("secondsTime").value;
+        timerData.black.totalTime = (+id("daysTime").value * 86400) + (+id("hoursTime").value * 3600) + (+id("minutesTime").value * 60) + +id("secondsTime").value;
+        timerData.increment = (+id("daysIncrement").value * 86400) + (+id("hoursIncrement").value * 3600) + (+id("minutesIncrement").value * 60) + +id("secondsIncrement").value;
+        
+        id("startButton").innerHTML = "Reset";
+        id("startButton").style.display = "none";
+        id("switchButton").style.display = "flex";
+        id("blackClock").style.rotate = "0deg";
+        id("timeSelector").style.display = "none";
+        id("tileRodeo").style.display = "none";
         
         timer = setInterval(function () {
             if (whitesTurn === true) {
-                timerData.whiteTotalTime--;
+                timerData.white.totalTime--;
             } else {
-                timerData.blackTotalTime--;
+                timerData.black.totalTime--;
             }
             updateClocks();
         }, 1000);
     } else {
         timerRunning = false;
+        whitesTurn = null;
+        id("startButton").innerHTML = "Start";
         clearInterval(timer);
     }
 }
 
-function updateClocks() {
-    let whiteDays = Math.floor(timerData.whiteTotalTime / 86400);
-    let whiteHours = Math.floor((timerData.whiteTotalTime % 86400) / 3600);
-    let whiteMinutes = Math.floor(((timerData.whiteTotalTime % 86400) % 3600) / 60);
-    let whiteSeconds = Math.floor(((timerData.whiteTotalTime % 86400) % 3600) / 60);
-
+function togglePlayer() {
+    if (whitesTurn === true) {
+        whitesTurn = false;
+        id("blackClock").style.rotate = "180deg";
+        id("whiteClock").style.rotate = "180deg";
+        id("switchButton").style.rotate = "180deg";
+        timerData.white.totalTime += timerData.increment;
+        updateClocks();
+    } else {
+        whitesTurn = true;
+        id("blackClock").style.rotate = "0deg";
+        id("whiteClock").style.rotate = "0deg";
+        id("switchButton").style.rotate = "0deg";
+        timerData.black.totalTime += timerData.increment;
+        updateClocks();
+    }
 }
 
+function updateClocks() {
+    timerData.white.days = Math.floor(timerData.white.totalTime / 86400);
+    timerData.white.hours = Math.floor((timerData.white.totalTime % 86400) / 3600);
+    timerData.white.minutes = Math.floor(((timerData.white.totalTime % 86400) % 3600) / 60);
+    timerData.white.seconds = Math.floor(((timerData.white.totalTime % 86400) % 3600) % 60);
+    timerData.black.days = Math.floor(timerData.black.totalTime / 86400);
+    timerData.black.hours = Math.floor((timerData.black.totalTime % 86400) / 3600);
+    timerData.black.minutes = Math.floor(((timerData.black.totalTime % 86400) % 3600) / 60);
+    timerData.black.seconds = Math.floor(((timerData.black.totalTime % 86400) % 3600) % 60);
+    setClocks(timerData.white.days, timerData.white.hours, timerData.white.minutes, timerData.white.seconds, timerData.black.days, timerData.black.hours, timerData.black.minutes, timerData.black.seconds);
+}   
+
 function setClocks(whiteDays, whiteHours, whiteMinutes, whiteSeconds, blackDays, blackHours, blackMinutes, blackSeconds) {
-    days = id("daysTime").value;
-    hours = id("hoursTime").value;
-    minutes = id("minutesTime").value;
-    seconds = id("secondsTime").value;
+    let newWhiteDays = whiteDays;
+    let newWhiteHours = whiteHours;
+    let newWhiteMinutes = whiteMinutes;
+    let newWhiteSeconds = whiteSeconds;
+    let newBlackDays = blackDays;
+    let newBlackHours = blackHours;
+    let newBlackMinutes = blackMinutes;
+    let newBlackSeconds = blackSeconds;
 
-    if (+id("daysTime").value === 0) {
-        days = "";
-    } else if (id("daysTime").value.length === 1) {
-        days = `0${days}:`;
+if (+blackDays === 0) {
+        newBlackDays = "";
+    } else if (blackDays.toString().length === 1) {
+        newBlackDays = `0${blackDays}:`;
     } else {
-        days = `${days}:`;
+        newBlackDays = `${blackDays}:`;
     }
 
-    if (+id("hoursTime").value === 0 && +id("daysTime").value === 0) {
-        hours = "";
-    } else if (id("hoursTime").value.length === 1) {
-        hours = `0${hours}:`;
+    if (+blackHours === 0 && +blackDays === 0) {
+        newBlackHours = "";
+    } else if (blackHours.toString().length === 1) {
+        newBlackHours = `0${blackHours}:`;
     } else {
-        hours = `${hours}:`;
+        newBlackHours = `${blackHours}:`;
     }
 
-    if (id("minutesTime").value.length === 1) {
-        minutes = `0${minutes}:`;
+    if (blackMinutes.toString().length === 1) {
+        newBlackMinutes = `0${blackMinutes}:`;
     } else {
-        minutes = `${minutes}:`;
+        newBlackMinutes = `${blackMinutes}:`;
     }
 
-    if (id("secondsTime").value.length === 1) {
-        seconds = `0${seconds}`;
+    if (blackSeconds.toString().length === 1) {
+        newBlackSeconds = `0${blackSeconds}`;
     } else {
-        seconds = `${seconds}:`;
+        newBlackSeconds = `${blackSeconds}`;
     }
 
-    id("blackTime").innerHTML = `${days}${hours}${minutes}${seconds}`;
-    id("whiteTime").innerHTML = `${days}${hours}${minutes}${seconds}`;
+    if (+whiteDays === 0) {
+        newWhiteDays = "";
+    } else if (whiteDays.toString().length === 1) {
+        newWhiteDays = `0${whiteDays}:`;
+    } else {
+        newWhiteDays = `${whiteDays}:`;
+    }
+
+    if (+whiteHours === 0 && +whiteDays === 0) {
+        newWhiteHours = "";
+    } else if (whiteHours.toString().length === 1) {
+        newWhiteHours = `0${whiteHours}:`;
+    } else {
+        newWhiteHours = `${whiteHours}:`;
+    }
+
+    if (whiteMinutes.toString().length === 1) {
+        newWhiteMinutes = `0${whiteMinutes}:`;
+    } else {
+        newWhiteMinutes = `${whiteMinutes}:`;
+    }
+
+    if (whiteSeconds.toString().length === 1) {
+        newWhiteSeconds = `0${whiteSeconds}`;
+    } else {
+        newWhiteSeconds = `${whiteSeconds}`;
+    }
+
+    id("blackTime").innerHTML = `${newBlackDays}${newBlackHours}${newBlackMinutes}${newBlackSeconds}`;
+    id("whiteTime").innerHTML = `${newWhiteDays}${newWhiteHours}${newWhiteMinutes}${newWhiteSeconds}`;
 }
 
 oninput = function (event) {
-    setClocks();
+    setClocks(id("daysTime").value, id("hoursTime").value, id("minutesTime").value, id("secondsTime").value, id("daysTime").value, id("hoursTime").value, id("minutesTime").value, id("secondsTime").value);
 }
